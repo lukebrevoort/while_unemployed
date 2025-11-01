1056c35456ee5546e4e86f7a1f5378075dacb7a8
lukebrevoort <luke@brevoort.com>
2025-10-31 22:43:30 -0400

    Fixing whole frontend changed, ran into conflicting issues with tailwind v3 and v4, resetting for proper setup

--- frontend/wu-frontend/src/lib/supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!, // processing api keys for supabase
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
-}
